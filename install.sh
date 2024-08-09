974ae72f330fed1208d60ff148267184a2b00bdd
guy <ethannathanduvall2013@gmail.com>
2024-08-09 15:11:43 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "==> Installing required packages..."
+sudo pacman -Sq --noconfirm hyprland alacritty waybar fish git base-devel
+echo "==> Installing yay..."
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+echo "==> Installing more required packages..."
+yay -Sq --noconfirm nitch neovim starship neovide
+echo "==> Planting dotfiles..."
+git clone https://github.com/starsprinter92/hyprdots/
+cd hyprdots
+cp -r .config/* ~/.config/
+echo "==> Installation done. Please restart."
+exit
