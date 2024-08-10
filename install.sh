61a778a43018bdc3ce3beee7af8de93c359976c6
guy <ethannathanduvall2013@gmail.com>
2024-08-09 20:07:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 echo "==> Installing more required packages..."
-yay -Sq --noconfirm nitch neovim starship neovide swww swayosd cava starship
+yay -Sq --noconfirm nitch neovim starship neovide swww swayosd cava starship rofi-wayland
 echo "==> Planting dotfiles..."
 git clone https://github.com/starsprinter92/hyprdots/
 cd hyprdots
