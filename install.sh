3f39cb120806db2dc21ba18250bc34cd5e47c64f
guy <ethannathanduvall2013@gmail.com>
2024-08-25 16:00:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 echo "==> Installing more required packages..."
-yay -Sq --noconfirm nitch neovim starship neovide swww swayosd cava starship rofi-wayland wezterm-git
+yay -Sq --noconfirm nitch neovim starship neovide swww swayosd cava starship rofi-wayland wezterm-git nwg-dcok-hyprland
 echo "==> Installing plugins..."
 hyprpm update
 hyprpm add https://github.com/hyprwm/hyprland-plugins
